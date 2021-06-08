c90ec5d34dd919c53a769a444d373f031de5d07e
benoit <cousinbenoit@hotmail.fr>
2021-06-08 10:31:55 +0200

    quest5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -4,17 +4,19 @@
 
 namespace App\Controller;
 
-
+use Doctrine\ORM\Mapping\Id;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-
+    /**
+    * @Route("/programs", name="program_")
+    */
 class ProgramController extends AbstractController
 
 {
     /**
-     * @Route("/program/", name="program_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
@@ -22,5 +24,18 @@ class ProgramController extends AbstractController
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Séries',
          ]);
+    }
+
+    /**
+     * @Route("/{id}", methods={"GET"}, name="show")
+     */
+    public function show(int $id): Response
+    {
+        if(is_int($id)){
+            return $this->render('program/show.html.twig', [
+                'id' => $id,
+            ]);
+        }
+
     }
 }
